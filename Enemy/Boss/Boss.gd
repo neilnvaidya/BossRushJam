a1183862d89d404ac794abf590eb93ad11d20633
neilvaidya <neilvaidya95@gmail.com>
2025-01-05 15:20:17 +0800

    boss virtual class

--- Enemy/Boss/Boss.gd
+++ Enemy/Boss/Boss.gd
@@ -3,10 +3,11 @@ class_name Boss
 
 extends CharacterBody2D
 
+
 var state = {
 }
 
-var required_state = {
+var required_states = {
 	idle = 0,
 	talk = 1,
 	move = 2,
@@ -19,19 +20,34 @@ var required_state = {
 	attack2_phase2 = 9
 }
 
-var optional_state = {}
+var optional_states = {}
 
-func _init(_required_state, _optional_state):
-	state = _required_state + optional_state
+var current_state
+
+func _init(_required_states = required_states, _optional_states = optional_states):
+	print("BOSSS")
+	state = _required_states.merged(optional_states)
+	current_state = state.idle
 	
 func _ready():
 	print(state)
 	
+	
 func _physics_process(delta):
-	pass
+	_on_state_tick(current_state)
 
 func _on_set_state(new_state):
-	pass
+	if new_state == null or new_state==current_state:
+		return
+	_on_state_exit(current_state)
+	_on_state_enter(new_state)
+	current_state = new_state
+	
+	
+func _on_state_tick(state):
+	if current_state ==null:
+		print("On State Tick - Current State Null")
+	
 
 func _on_state_enter(state):
 	pass
--- Enemy/Boss/boss_prefab.gd
+++ Enemy/Boss/boss_prefab.gd
@@ -1,6 +1,12 @@
-extends Node
+extends Boss
 
-
+func _init():
+	optional_states = {optional1 = 10}
+	super(required_states, optional_states)
+	
+	#print("prefab")
+	#print(state)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +15,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_set_state(new_state):
+	super(new_state)
+	
+	
+func _on_state_tick(state):
+	super(state)
+	
+
+func _on_state_enter(state):
+	super(state)
+
+func _on_state_exit(state):
+	super(state)
