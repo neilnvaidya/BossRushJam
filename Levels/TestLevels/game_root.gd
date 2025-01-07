1583527ce195fd157e9c5bed100da2985e5f797e
neilvaidya <neilvaidya95@gmail.com>
2025-01-07 20:24:27 +0800

    test level, door example, audio cleanup

---
+++ Levels/TestLevels/game_root.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# SgtChilli Test Level (Acting as Game Root)
+
+@onready var player : CharacterBody2D = $Player
+@onready var camera : Camera2D = $Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	camera.position = player.position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	camera.position = player.position
