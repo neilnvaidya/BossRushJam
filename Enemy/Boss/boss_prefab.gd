c1f1c3130f6905362568044c098d6c03bd0e535b
neilvaidya <neilvaidya95@gmail.com>
2025-01-07 21:28:21 +0800

    boss virtual done, orb boss WIP

--- Enemy/Boss/boss_prefab.gd
+++ Enemy/Boss/boss_prefab.gd
@@ -1,30 +1,26 @@
 extends Boss
 
+@onready var anim_player :AnimationPlayer = $AnimationPlayer
+@onready var sprite : Sprite2D = $Sprite
+@onready var collider : CollisionShape2D = $CollisionShape2D
+
 func _init():
-	optional_states = {optional1 = 10}
-	super(required_states, optional_states)
-	
-	#print("prefab")
-	#print(state)
+	super()
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	super()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-func _on_set_state(new_state):
+func _set_state(new_state):
 	super(new_state)
-	
 	
 func _on_state_tick(state):
 	super(state)
 	
-
 func _on_state_enter(state):
 	super(state)
 
