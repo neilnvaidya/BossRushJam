f6085ae1f4fcd5b262cdcbdaab1822606a51624b
Koua Thao <kthao91@live.com>
2025-01-09 18:53:36 -0600

    test

--- Levels/game_world.gd
+++ Levels/game_world.gd
@@ -1,1 +1,4 @@
 extends Node2D
+
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var player: Player = $Player
--- game.gd
+++ game.gd
@@ -1,5 +1,6 @@
 class_name Run
 extends Node2D
+
 const TEST_YOYO = preload("res://yoyos/test_yoyo.tres")
 var player_stats: PlayerStats
 @onready var player = $Player
