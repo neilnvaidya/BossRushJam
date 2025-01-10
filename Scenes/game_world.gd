2901a83077d7674f75d33e0339bad8bb493d964d
Koua Thao <kthao91@live.com>
2025-01-10 01:14:29 -0600

    fix collision on lvl 1
    fix collision on lvl 1

--- Scenes/game_world.gd
+++ Scenes/game_world.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var player: Player = $Player
 
 @export var start_scene:String
+@onready var mark: Marker2D = $Mark
+
 
 #need to clear the node after lvl is finish
 var current_level:Node2D
@@ -15,18 +17,24 @@ func _ready():
 	$Levels.add_child(current_level)
 	current_level.connect("go_to_level",Callable(self, "_on_go_to_level"))
 	camera.position = player.position
-
+	spawn_point()
 	
 func _physics_process(delta):
 	camera.position = player.position
-
+	print(mark)
+	
 func _on_go_to_level(scene:PackedScene):
 	print("game world")
 	var new_level=scene.instantiate()
 	$Levels.add_child(new_level)
-
+	spawn_point()
 	old_level=current_level
 	new_level.connect("go_to_level",Callable(self,"_on_goto_level"))
 	current_level=new_level
 	get_tree().paused=false
 	old_level.queue_free()
+
+func spawn_point():
+	print(mark)
+	if mark:
+		player.position = mark.position
